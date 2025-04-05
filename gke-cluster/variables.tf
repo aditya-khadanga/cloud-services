b67f1e07ef10f018088ae2e46b5c19bb676ff150
Aditya Khadanga <97084694+aditya-khadanga@users.noreply.github.com>
2025-04-05 18:25:08 +0530

    Update variables.tf
    6th commit

---
+++ gke-cluster/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "asia-south1"
+}
+
+variable "zone" {
+  type    = string
+  default = "asia-south1-a"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "my-gke-cluster"
+}
+                     
